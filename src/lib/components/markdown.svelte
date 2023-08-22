a5d435fe43c605ff4c953e07cd385d91c2c2f0c4
Michael Pollente <pollentem@gmail.com>
2023-08-22 16:22:55 +0800

    Initial commit

---
+++ src/lib/components/markdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let file: string | null
+</script>
+
+{#if file}
+	<section data-markdown={file} />
+{:else}
+	<section data-markdown>
+		<div data-template>
+			<slot />
+		</div>
+	</section>
+{/if}
